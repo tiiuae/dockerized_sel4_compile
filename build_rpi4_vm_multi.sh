15a4b6df5020f14af26cd6bac2ab6e5b4ea07d38
Jouni Raunio <jouni.raunio@unikie.com>
2021-05-27 14:40:14 +0300

    Added separate scripts for building demos.

---
+++ build_rpi4_vm_multi.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+set -e
+
+CAMKES_VM_APP=vm_multi
+PLATFORM=rpi4
+
+TARGET=${PLATFORM}_${CAMKES_VM_APP}
+
+rm -rf ${TARGET}
+./init-build.sh -B ${TARGET} -DAARCH64=1 -DPLATFORM=${PLATFORM} -DCAMKES_VM_APP=${CAMKES_VM_APP} -DCROSS_COMPILER_PREFIX=aarch64-linux-gnu-
+cd ${TARGET}
+ninja
+
+echo "Here are your binaries in ${TARGET}/images: "
+ls -l ./images
+
